65271bd6a3e34aeacb52b24af5e400209e093a45
Ntepp Jean Joel <ntjoel19@gmail.com>
2023-02-28 13:01:56 +0100

    Working on css : background colors, reset.css ans colors

--- js/componants/main/card.js
+++ js/componants/main/card.js
@@ -1,11 +1,13 @@
 
 export const card = (index, session) => {
     let div = `
-            <div class="task_card dflex dflex_c dflex_spacae_betw">
-                <span>${session.type}</span>
-                <span>${session.recipient.displayableName}</span>  
+        <a href="">
+            <div class="task_card dflex dflex_c dflex_space_betw">
+                <h3>${session.type}</h3>
+                <em>${session.recipient.displayableName}</em>  
                 <span>${session.sessionDate.split("T")[0]} • ${session.sessionDate.split("T")[1].split("+")[0]}</span>
             </div>
+        </a>
     `
 
     return div
--- js/componants/main/column.js
+++ js/componants/main/column.js
@@ -4,13 +4,13 @@ export const column = (index, sessionList) => {
     let title = ""
     switch (index) {
         case 0:
-            title = "Comming Soon Sessions("+sessionList.length+")"
+            title = "<i class='fa-regular fa-square-caret-right'></i> <span>Coming Soon</span><span>"+sessionList.length+"<span>"
             break;
         case 1:
-            title = "Pending Sessions("+sessionList.length+")"
+            title = "<i class='fa-solid fa-spinner'></i><span>Pending</span><span>"+sessionList.length+"</span>"
             break;
         case 2:
-            title = "Completed Sessions("+sessionList.length+")"
+            title = "<i class='fa-regular fa-circle-check'></i><span>Completed</span><span>"+sessionList.length+"+</span>"
             break;
         default:
             break;
@@ -23,8 +23,8 @@ export const column = (index, sessionList) => {
 
     const section = `
         <section class="tasks_section dflex dflex_c">
-            <h2 class="dflex dflex_space_betw">${title} </h2>
-            <div>
+            <h2 class="column_title dflex dflex_space_betw">${title} </h2>
+            <div class="tasks_list">
                 ${cardList}
             </div>
         </section>
